2557e29fb56b6e3d0cb4b580e71e4c5b0c1d05f6
Shaheen Hussain <shaheen36.saa@gmail.com>
2026-05-04 14:25:51 +0400

    Added the tasks from 5 to 9

--- Queries-Advanced.sql
+++ Queries-Advanced.sql
@@ -44,3 +44,65 @@ INNER JOIN FlightCrew fc ON c.CrewID = fc.CrewID
 GROUP BY c.CrewID, c.FullName 
 HAVING COUNT(fc.FlightID) > 1;
 
+
+
+select f.FlightNumber, AVG(b.Price) as AveragePrice
+from Flights f
+inner join Bookings b On f.FlightID = b.FlightID
+group by f.FlightNumber
+having AVG(b.Price)> (select AVG(price) from Bookings)
+
+
+
+select top 1
+f.FlightNumber, 
+o.AirportName as OriginAirportName,
+d.AirportName as DestinationAirportName,
+COUNT(b.BookingID) as NumOfBooking
+from Flights f
+inner join Bookings b On f.FlightID = b.FlightID
+inner join Airports o On f.OriginCode = o.IATA_Code
+inner join Airports d On f.DestinationCode = d.IATA_Code
+group by f.FlightNumber, o.AirportName,d.AirportName
+order by NumOfBooking desc
+
+
+
+select sum(b.Price) as TotalRevenue,
+COUNT(b.BookingID) as NumOfBooking,
+AVG(b.Price) as AveragePrice,
+MAX(b.Price) as HighestPrice,
+MIN(b.Price) as LowestPrice
+from Bookings b
+group by b.Class
+
+
+select 
+	p.FullName as PassengerName,
+	f.FlightNumber,
+	b.BookingDate
+from Bookings b
+inner join Flights f On b.FlightID = f.FlightID
+inner join Passengers p On b.PassengerID = p.PassengerID
+where f.Status = 'Cancelled'
+
+
+select 
+	f.FlightNumber,
+	COUNT(fc.CrewID) as NumberOfCrewMembers,
+	f.DepartureDateTime
+
+from Flights f
+inner join FlightCrew fc On f.FlightID =fc.FlightID
+where exists(
+	select 1 from FlightCrew fc1
+	inner join CrewMembers c1 On fc1.CrewID = c1.CrewID
+	where fc1.FlightID = f.FlightID and c1.Role = 'Pilot')
+And Exists(
+	select 1 from FlightCrew fc2
+	inner join CrewMembers c2 On fc2.CrewID = c2.CrewID
+	where fc2.FlightID = f.FlightID and c2.Role = 'Flight Attendant')
+group by f.FlightNumber,f.DepartureDateTime
+
+
+
