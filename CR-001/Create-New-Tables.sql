d79505a971c26bdaf168680046c3712c92671a7c
Shaheen Hussain <shaheen36.saa@gmail.com>
2026-05-05 10:58:38 +0400

    Added the create-new-tables.sql file

---
+++ CR-001/Create-New-Tables.sql
@@ -0,0 +1,50 @@
+use Airport_System
+
+Create Table Airline (AirlineId int primary key identity(1,1),
+					  IATA_Airline char(3) unique not null ,
+					  AirlineFullName varchar(100) unique not null ,
+					  Country_Of_Registration varchar(100) not null,
+					  Contact_Email varchar(100) unique not null)
+
+
+Create Table Gate (GateID int primary key identity(1,1),
+				   Gate_Code varchar(10) not null,
+				   terminal varchar(50) not null,
+				   airport_id char(3) not null,
+				   Constraint FK_Gate_Airport Foreign key (airport_id)
+						REFERENCES Airports(IATA_Code),
+				   Constraint UQ_Gate_Airport UNIQUE (Gate_Code,airport_id))
+
+
+Create Table Baggage (Baggage_id int primary key identity(1,1),
+					  Tag_Number varchar(50) unique not null,
+					  Weight_kg decimal(5,2) not null check (Weight_kg >0),
+					  type varchar(10) not null check(type in ('Cabin', 'Checked')),
+					  booking_id int not null,
+					  Constraint fk_baggage_booking foreign key (booking_id)
+						REFERENCES Bookings(BookingID)
+						On Delete Cascade On Update Cascade)
+
+Create Table FlightDelayLog (delay_id int primary key identity (1,1),
+							reason varchar (MAX) not null,
+							duration_minutes int not null check(duration_minutes > 0),
+							recorded_at datetime not null,
+							flight_id int not null,
+							constraint fk_FlightDelayLog_flight foreign key (flight_id)
+								REFERENCES Flights(FlightID)
+								On delete Cascade On Update Cascade)
+
+
+
+Alter table Flights add airline_id int, gate_id int
+
+alter table Flights add constraint fk_Flights_airline 
+			foreign key (airline_id) 
+			REFERENCES Airline(AirlineId) 
+			On delete no action on update no action
+
+Alter table Flights add 
+		constraint fk_Flights_gate foreign key (gate_id)
+		References Gate(GateID)
+		on delete set null on update cascade
+	
