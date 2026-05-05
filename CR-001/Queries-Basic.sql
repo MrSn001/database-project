4d3f64fc3c7eadb07dd2b0dc3a4dbd2d6c70f99c
Shaheen Hussain <shaheen36.saa@gmail.com>
2026-05-05 12:13:22 +0400

    Added the Queries-basic.sql File

---
+++ CR-001/Queries-Basic.sql
@@ -0,0 +1,24 @@
+--1
+select AirlineFullName,Country_Of_Registration from Airline
+order by Country_Of_Registration asc
+
+--2
+select g.Gate_Code,a.AirportName from Gate g
+inner join Airports a On g.airport_id =a.IATA_Code
+
+--3
+select type,Weight_kg 
+from Baggage
+order by Weight_kg desc
+
+--4
+select f.FlightNumber,fdl.recorded_at from FlightDelayLog fdl
+inner join Flights f On fdl.flight_id = f.FlightID
+order by fdl.recorded_at
+
+--5
+select FlightNumber, Status, OriginCode,DestinationCode 
+from Flights 
+where gate_id is null
+
+
