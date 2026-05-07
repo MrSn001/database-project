226be0e8eb3491e04c4ea54868c20423d5bbe458
Shaheen Hussain <shaheen36.saa@gmail.com>
2026-05-07 13:18:59 +0400

    Added the last 2 tasks (5-6)

--- CR-001/Queries-Advanced.sql
+++ CR-001/Queries-Advanced.sql
@@ -46,3 +46,46 @@ from Baggage b
 inner join Bookings bk On b.booking_id = bk.BookingID
 inner join Passengers p On bk.PassengerID = p.PassengerID
 group by p.FullName,b.type
+
+
+
+--5
+select 
+f.FlightNumber, 
+o.AirportName as OriginAirport ,
+d.AirportName as DestinationAirport,
+SUM(b.Price) as TotalRevenue 
+
+from Flights f
+inner join Airline al On f.airline_id = al.AirlineId
+inner join Airports o On f.OriginCode = o.IATA_Code
+inner join Airports d On f.DestinationCode = d.IATA_Code
+inner join Bookings b On f.FlightID = b.FlightID
+where al.Country_Of_Registration = 'Oman'
+group by f.FlightNumber,o.AirportName,d.AirportName
+
+
+
+--6
+SELECT 
+    f.FlightNumber,
+    al.AirlineFullName,
+    ISNULL(g.Gate_Code, 'N/A') AS GateCode,
+    o.City AS OriginCity,
+    d.City AS DestinationCity,
+    COUNT(DISTINCT bk.BookingID) AS TotalPassengersBooked,
+    COUNT(DISTINCT b.Baggage_id) AS TotalBaggageItems,
+    -- Using a Subquery for Delays ensures we don't multiply durations by passengers
+    ISNULL((SELECT SUM(duration_minutes) FROM FlightDelayLog WHERE flight_id = f.FlightID), 0) AS TotalDelayDuration,
+    SUM(bk.Price) AS TotalRevenue -- Revenue is safe here because we group by flight
+FROM Flights f
+INNER JOIN Airline al ON f.airline_id = al.AirlineId
+INNER JOIN Airports o ON f.OriginCode = o.IATA_Code
+INNER JOIN Airports d ON f.DestinationCode = d.IATA_Code
+LEFT JOIN Gate g ON f.gate_id = g.GateID -- Left join so we don't hide flights without gates
+LEFT JOIN Bookings bk ON f.FlightID = bk.FlightID
+LEFT JOIN Baggage b ON bk.BookingID = b.booking_id
+GROUP BY f.FlightID, f.FlightNumber, al.AirlineFullName, g.Gate_Code, o.City, d.City
+ORDER BY TotalRevenue DESC; -- Requirement: Highest to lowest revenue
+
+
